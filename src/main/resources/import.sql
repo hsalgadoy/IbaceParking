3ec79e6887782f3f6a71e147e04fb1ebc1d27bf0
hsneider.salgado <hsneider.salgado@HSNEIDER-SALGADO.ceiba.local>
2019-07-21 19:59:58 -0500

    h2 file update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-insert into vehicle_type_entity (id_vehicle_type, day_value,type_description,extra_cost,hour_value,parking_space,space_aviable) values(1,4000,'moto',2000,400,10,10);
+insert into vehicle_type_entity (id_vehicle_type, day_value,type_description,extra_cost,hour_value,parking_space,space_aviable) values(1,4000,'moto',2000,500,10,10);
+insert into vehicle_type_entity (id_vehicle_type, day_value,type_description,extra_cost,hour_value,parking_space,space_aviable) values(2,8000,'carro',0,1000,20,20);
